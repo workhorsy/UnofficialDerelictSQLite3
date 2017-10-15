b24e7395baac9f7e1ab1f0b64b8780b340de5fc8
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-10-15 16:33:17 -0700

    Release 0.2.0

--- tools/make_release.sh
+++ tools/make_release.sh
@@ -2,16 +2,18 @@
 # Stop and exit on error
 set -e
 
-VERSION="0.1.0"
+VERSION="0.2.0"
 
 cd ..
 sed 's/$VERSION/'$VERSION'/g' tools/README.template.md > README.md
 
 # Generate documentation
+<<TURN_OFF_DOCS
 dub --build=docs
 mkdir docs/$VERSION
 mv docs/DerelictSQLite3.html docs/$VERSION/index.html
 git add docs/$VERSION/
+TURN_OFF_DOCS
 
 # Create release
 git commit -a -m "Release $VERSION"
