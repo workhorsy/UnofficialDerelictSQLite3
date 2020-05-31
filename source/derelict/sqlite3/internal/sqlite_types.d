a3d70b77a7e8ae4b2d385fdf8244c1f4f04ffc1e
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2020-05-30 22:35:15 -0700

    Fixed Bug #2: Make tests run without main for changes in dmd 2.090

--- source/derelict/sqlite3/internal/sqlite_types.d
+++ source/derelict/sqlite3/internal/sqlite_types.d
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// Copyright (c) 2017-2020 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 // Boost Software License - Version 1.0
 // Dynamic bindings to the SQLite library for the D Programming Language
 // https://github.com/workhorsy/UnofficialDerelictSQLite3
@@ -16,7 +16,7 @@ alias Sint64 = long;
 alias Uint64 = ulong;
 
 struct sqlite3 {
-	
+
 }
 
 // sqlite3.h
--- source/derelict/sqlite3/sqlite.d
+++ source/derelict/sqlite3/sqlite.d
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// Copyright (c) 2017-2020 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 // Boost Software License - Version 1.0
 // Dynamic bindings to the SQLite library for the D Programming Language
 // https://github.com/workhorsy/UnofficialDerelictSQLite3
